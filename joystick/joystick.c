649f9ff2827d7c1bc19c0e3c12754c20eecd416d
Michele Fabbri <fabbrimichele75@gmail.com>
2024-11-15 21:47:04 +0100

    Joysting FIRE check fixed

--- joystick/joystick.c
+++ joystick/joystick.c
@@ -9,26 +9,28 @@
 #define IKBD_JOY_DOWN    0x02
 #define IKBD_JOY_LEFT    0x04
 #define IKBD_JOY_RIGHT   0x08
-#define IKBD_JOY_FIRE    0x10
+#define IKBD_JOY_FIRE    0x80
 
 #define IKBD_JOY_EVENT_REPORTING 0x14
 
 void install_joy_vector(void);
 void uninstall_joy_vector(void);
 void joy_vector(void* status);
-void print_joy_status(void);
+void print_joy_status(unsigned short status);
 
 unsigned short joy_status;
 
 int main() {
-    unsigned short old_joy_status;
+    unsigned short old_joy_status = 0;
 
     Supexec(install_joy_vector);
     Bconout(DEV_IKBD, IKBD_JOY_EVENT_REPORTING);
 
+    printf("Move joystick...\n");
+
 	while (true) {
         if (joy_status != old_joy_status) {
-            print_joy_status();
+            print_joy_status(joy_status);
             old_joy_status = joy_status;
         }
 	}
@@ -37,18 +39,22 @@ int main() {
     return 0;
 }
 
-void print_joy_status(void) {
-    if (joy_status & IKBD_JOY_UP) {
+// Status needs to be passed to make a copy, otherwise
+// it could change during the execution of print_joy_status()
+void print_joy_status(unsigned short status) {
+    printf("Status: %04x\n", status);
+
+    if (status & IKBD_JOY_UP) {
         printf("UP\n");
-    } else if (joy_status & IKBD_JOY_DOWN) {
+    } else if (status & IKBD_JOY_DOWN) {
         printf("DOWN\n");
-    } else if (joy_status & IKBD_JOY_LEFT) {
+    } else if (status & IKBD_JOY_LEFT) {
         printf("LEFT\n");
-    } else if (joy_status & IKBD_JOY_RIGHT) {
+    } else if (status & IKBD_JOY_RIGHT) {
         printf("RIGHT\n");
     }
 
-    if (joy_status & IKBD_JOY_FIRE) {
+    if (status & IKBD_JOY_FIRE) {
         printf("FIRE\n");
     }
 }
