76b8433ac1424c35ca6b3b4b798db6a04900ab8b
Michele Fabbri <fabbrimichele75@gmail.com>
2025-03-23 20:27:29 +0100

    Implement read and write to memory

--- monitor/monitor.c
+++ monitor/monitor.c
@@ -1,7 +1,72 @@
 #include <stdio.h>
 
+void help();
+void read();
+void write();
+
 int main() {
-   printf("Monitor v0.1\r\n");
-   printf(">\r\n");
+   printf("\r\nMonitor v0.1\r\n");
+   printf("Press 'h' for help.\r\n\r\n");
+
+   int c = 0;
+   while(c != 'q' && c != 'Q') {
+    printf("command> ");
+    c = getch();
+    printf("%c\r\n", c);
+
+    switch (toupper(c)) {
+        case 'H':
+            help();
+            break;
+
+        case 'R':
+            read();
+            break;
+
+        case 'W':
+            write();
+            break;
+    
+        default:
+            break;
+    }    
+   }
+
+   printf("\r\n");
    return 0;
 }
+
+void help() {
+    printf("[r]ead: read memory\r\n");
+    printf("[h]elp: this help\r\n");
+    printf("[q]uit: exit program\r\n");
+}
+
+void read() {
+    unsigned int input_addr;
+    char* addr;    
+    
+    printf("read from (hex): ");
+    scanf("%x", &input_addr);
+    printf("\r\n");
+    addr = (char*) input_addr;
+
+    printf("val       (hex): %02x\r\n", (unsigned char)*addr);
+}
+
+void write() {
+    unsigned int input_addr;
+    char* addr;
+    unsigned int val;
+    
+    printf("write to  (hex): ");
+    scanf("%x", &input_addr);
+    printf("\r\n");
+    addr = (char*) input_addr;
+
+    printf("byte val  (hex): ");
+    scanf("%x", &val);
+    printf("\r\n");    
+    *addr = (char) val;
+}
+
